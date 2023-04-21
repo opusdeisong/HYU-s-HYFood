da321dd3a5938b3b81d9e3faa728f8b1be691136
Jina Kim <drjina02@gmail.com>
2023-04-21 15:51:57 +0900

    Restrict map image dragging within container bounds
    This commit modifies the map dragging functionality to prevent the map image from being dragged outside its container. It ensures that the map image stays within the bounds of the map container, providing a better user experience and preventing any accidental dragging issues.

--- app.js
+++ app.js
@@ -81,6 +81,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
       translation.x += dx;
       translation.y += dy;
+
+      // 지도 이미지가 지정된 범위 내에서만 드래그할 수 있도록 제한
+      translation.x = Math.min(0, Math.max(translation.x, mapContainer.clientWidth - mapImage.clientWidth * scale));
+      translation.y = Math.min(0, Math.max(translation.y, mapContainer.clientHeight - mapImage.clientHeight * scale));
+
       mapImage.style.transform = `translate(${translation.x}px, ${translation.y}px) scale(${scale})`;
 
       previousMousePosition = { x: e.clientX, y: e.clientY };
